e6f0a9d3cc4d697b8d14033236ad33a3fb5e0597
Rebornian48 <80138640+Rebornian48@users.noreply.github.com>
2024-01-21 12:03:28 +0700

    refresh version

--- js/script.js
+++
@@ -1,6 +0,0 @@
-const darkModeToggle = document.getElementById('dark-mode-toggle');
-const body = document.body;
-
-darkModeToggle.addEventListener('change', () => {
-    body.classList.toggle('dark');
-});
